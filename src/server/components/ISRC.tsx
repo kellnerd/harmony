36992a936dcd1a2a7d6690275ab518920b6fd4ca
David Kellner <52860029+kellnerd@users.noreply.github.com>
2023-04-29 19:07:21 +0200

    web: Create a component to display an ISRC

---
+++ src/server/components/ISRC.tsx
@@ -0,0 +1,16 @@
+const isrcPattern = /^([A-Z]{2})-?([A-Z0-9]{3})-?(\d{2})-?(\d{5})$/;
+
+export function ISRC({ code }: { code: string }) {
+	const codeMatch = code.trim().match(isrcPattern);
+
+	return codeMatch
+		? (
+			<code class='isrc'>
+				<span class='country'>{codeMatch[1]}</span>
+				<span class='registrant'>{codeMatch[2]}</span>
+				<span class='year'>{codeMatch[3]}</span>
+				<span class='designation'>{codeMatch[4]}</span>
+			</code>
+		)
+		: <code class='invalid-isrc'>{code}</code>;
+}
--- src/server/components/Tracklist.tsx
+++ src/server/components/Tracklist.tsx
@@ -1,4 +1,5 @@
 import { ArtistCredit } from './ArtistCredit.tsx';
+import { ISRC } from './ISRC.tsx';
 import { formatDuration } from '../../utils/time.ts';
 
 import type { HarmonyMedium } from '../../harmonizer/types.ts';
@@ -22,7 +23,9 @@ export function Tracklist({ medium }: { medium: HarmonyMedium }) {
 						<td>{track.title}</td>
 						<td>{track.artists && <ArtistCredit artists={track.artists} />}</td>
 						<td>{formatDuration(track.duration)}</td>
-						<td>{track.isrc}</td>
+						<td>
+							{track.isrc && <ISRC code={track.isrc} />}
+						</td>
 					</tr>
 				);
 			})}
