32f47bdc96657638be6c8e1dcc945cda93615f9e
David Kellner <52860029+kellnerd@users.noreply.github.com>
2023-06-06 21:26:14 +0200

    web: Overlay GTIN and URL lookup inputs with icons

--- src/server/components/ReleaseLookup.tsx
+++ src/server/components/ReleaseLookup.tsx
@@ -1,3 +1,7 @@
+import InputWithOverlay from './InputWithOverlay.tsx';
+import IconBarcode from 'tabler-icons/barcode.tsx';
+import IconWorldWww from 'tabler-icons/world-www.tsx';
+
 import type { GTIN } from '../../harmonizer/types.ts';
 
 export default function ReleaseLookup({ gtin, externalUrl }: {
@@ -10,11 +14,15 @@ export default function ReleaseLookup({ gtin, externalUrl }: {
 				<label for='gtin-input'>
 					<abbr title='Global Trade Item Number'>GTIN</abbr>:
 				</label>
-				<input type='text' name='gtin' id='gtin-input' value={gtin ?? ''} />
+				<InputWithOverlay type='text' name='gtin' id='gtin-input' value={gtin ?? ''}>
+					<IconBarcode />
+				</InputWithOverlay>
 			</div>
 			<div>
 				<label for='url-input'>URL:</label>
-				<input type='text' name='url' id='url-input' value={externalUrl ?? ''} />
+				<InputWithOverlay type='text' name='url' id='url-input' value={externalUrl ?? ''}>
+					<IconWorldWww />
+				</InputWithOverlay>
 			</div>
 			<button type='submit'>Lookup</button>
 		</form>
