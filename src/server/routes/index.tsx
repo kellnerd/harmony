2aa5eead8a6d1908eba6b650e53791df69ac4bfc
David Kellner <52860029+kellnerd@users.noreply.github.com>
2023-06-11 12:40:09 +0200

    web: Fill index page with header, supported providers and release lookup

--- src/server/routes/index.tsx
+++ src/server/routes/index.tsx
@@ -1,3 +1,7 @@
+import { MessageBox } from '../components/MessageBox.tsx';
+import ReleaseLookup from '../components/ReleaseLookup.tsx';
+
+import { providerNames } from '../../providers.ts';
 import { Head } from 'fresh/runtime.ts';
 
 export default function Home() {
@@ -5,10 +9,15 @@ export default function Home() {
 		<>
 			<Head>
 				<title>Harmony</title>
+				<link rel='stylesheet' href='harmony.css' />
 			</Head>
-			<div>
-				<p>Welcome to Harmony!</p>
-			</div>
+			<header class='center'>
+				<h1>Harmony</h1>
+				<p class='subtitle'>Music Metadata Aggregator and MusicBrainz Importer</p>
+			</header>
+			<MessageBox message={{ text: `Supported providers: ${providerNames.join(', ')}`, type: 'info' }} />
+			<h2 class='center'>Release Lookup</h2>
+			<ReleaseLookup formAction='release' />
 		</>
 	);
 }
