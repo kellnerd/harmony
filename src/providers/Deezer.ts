97b35d818bb8f6efa11dad3442ec73d4888b0f4e
David Kellner <52860029+kellnerd@users.noreply.github.com>
2023-12-09 17:33:54 +0100

    provider: Copy release lookup logic into separate classes verbatim
    These still depend on the respective MetadataProvider for general provider features.

--- src/providers/Deezer.ts
+++ src/providers/Deezer.ts
@@ -1,4 +1,4 @@
-import { DurationPrecision, MetadataProvider, ProviderOptions } from './abstract.ts';
+import { DurationPrecision, MetadataProvider, ProviderOptions, ReleaseLookup } from './abstract.ts';
 import { availableRegions } from './regions/Deezer.ts';
 import { parseHyphenatedDate, PartialDate } from '../utils/date.ts';
 import { ResponseError } from '../utils/errors.ts';
@@ -214,13 +214,191 @@ export default class DeezerProvider extends MetadataProvider<Release> {
 		);
 	}
 
-	private async query(apiUrl: URL) {
+	async query(apiUrl: URL) {
 		const data = await this.fetchJSON(apiUrl);
 
 		if (data.error) {
 			throw new DeezerResponseError(data.error, apiUrl);
 		}
 		return data;
+	}
+}
+
+export class DeezerReleaseLookup extends ReleaseLookup<DeezerProvider> {
+
+	readonly supportedUrls = new URLPattern({
+		hostname: 'www.deezer.com',
+		pathname: String.raw`/:region(\w{2})?/album/:id(\d+)`,
+	});
+
+	constructReleaseUrl(id: string): URL {
+		return new URL(id, 'https://www.deezer.com/album/');
+	}
+
+	constructReleaseApiUrl({ lookup }: RawReleaseOptions): URL | undefined {
+		if (lookup.method === 'gtin') {
+			return new URL(`album/upc:${lookup.value}`, this.provider.apiBaseUrl);
+		} else if (lookup.method === 'id') {
+			return new URL(`album/${lookup.value}`, this.provider.apiBaseUrl);
+		}
+	}
+
+	protected async getRawRelease(options: RawReleaseOptions): Promise<RawResult<Release>> {
+		const apiUrl = this.constructReleaseApiUrl(options)!;
+
+		return {
+			data: await this.provider.query(apiUrl),
+			lookupInfo: options.lookup,
+		};
+	}
+
+	private async getRawTracklist(albumId: string): Promise<TracklistItem[]> {
+		const tracklist: TracklistItem[] = [];
+		let nextPageQuery: string | undefined = `album/${albumId}/tracks`;
+
+		while (nextPageQuery) {
+			const response: Response<TracklistItem> = await this.provider.query(new URL(nextPageQuery, this.provider.apiBaseUrl));
+			tracklist.push(...response.data);
+			nextPageQuery = response.next;
+		}
+
+		return tracklist;
+	}
+
+	protected getRawTrackById(trackId: string): Promise<Track> {
+		return this.provider.query(new URL(`track/${trackId}`, this.provider.apiBaseUrl));
+	}
+
+	protected async convertRawRelease(
+		{ data: rawRelease, lookupInfo }: RawResult<Release>,
+		options: RawReleaseOptions,
+	): Promise<HarmonyRelease> {
+		const id = rawRelease.id.toString();
+		const needToFetchIndividualTracks = options.withAllTrackArtists || options.withAvailability || false;
+		const needToFetchDetailedTracklist = !needToFetchIndividualTracks &&
+			(options.withSeparateMedia || options.withISRC || false);
+
+		let rawTracklist: Array<ReleaseTrack | TracklistItem | Track>;
+		let media: HarmonyMedium[];
+
+		if (needToFetchDetailedTracklist) {
+			rawTracklist = await this.getRawTracklist(id);
+		} else {
+			rawTracklist = rawRelease.tracks.data;
+
+			if (needToFetchIndividualTracks) {
+				// replace minimal tracklist with all available details for each track
+				rawTracklist = await Promise.all(rawTracklist.map((track) => this.getRawTrackById(track.id.toString())));
+			}
+		}
+
+		if (needToFetchDetailedTracklist || needToFetchIndividualTracks) {
+			// we have enough info to split the tracklist into multiple media
+			media = this.convertRawTracklist(rawTracklist as Array<TracklistItem | Track>);
+		} else {
+			media = [{
+				format: 'Digital Media',
+				tracklist: rawTracklist.map(this.convertRawTrack.bind(this)),
+			}];
+		}
+
+		const releaseUrl = new URL(rawRelease.link);
+
+		return {
+			title: rawRelease.title,
+			artists: rawRelease.contributors.map(this.convertRawArtist.bind(this)),
+			gtin: rawRelease.upc,
+			externalLinks: [{
+				url: releaseUrl,
+				types: ['free streaming'],
+			}],
+			media,
+			releaseDate: parseHyphenatedDate(rawRelease.release_date),
+			// split label string using slashes if the results have at least 3 characters
+			labels: rawRelease.label.split(/(?<=[^/]{3,})\/(?=[^/]{3,})/).map((label) => ({
+				name: label.trim(),
+			})),
+			status: 'Official',
+			packaging: 'None',
+			images: [{
+				url: new URL(rawRelease.cover_xl),
+				thumbUrl: new URL(rawRelease.cover_medium),
+				types: ['front'],
+			}],
+			availableIn: this.determineAvailability(media),
+			info: this.generateReleaseInfo({ id, lookupInfo, options }),
+		};
+	}
+
+	private convertRawTracklist(tracklist: Array<TracklistItem | Track>): HarmonyMedium[] {
+		const result: HarmonyMedium[] = [];
+		let medium: HarmonyMedium = {
+			tracklist: [],
+		};
+
+		// split flat tracklist into media
+		tracklist.forEach((item, index) => {
+			// store the previous medium and create a new one
+			if (item.disk_number !== medium.number) {
+				if (medium.number) {
+					result.push(medium);
+				}
+
+				medium = {
+					number: item.disk_number,
+					format: 'Digital Media',
+					tracklist: [],
+				};
+			}
+
+			medium.tracklist.push(this.convertRawTrack(item, index));
+		});
+
+		// store the final medium
+		result.push(medium);
+
+		return result;
+	}
+
+	private convertRawTrack(track: ReleaseTrack | TracklistItem | Track, index: number): HarmonyTrack {
+		const result: HarmonyTrack = {
+			number: index + 1,
+			title: track.title,
+			duration: track.duration * 1000,
+		};
+
+		if ('isrc' in track) {
+			// this is a detailed tracklist item
+			result.isrc = track.isrc;
+			result.number = track.track_position;
+		}
+
+		if ('contributors' in track) {
+			// all available details about this track have been fetched
+			result.artists = track.contributors.map(this.convertRawArtist);
+			result.availableIn = track.available_countries;
+		} else {
+			result.artists = [this.convertRawArtist(track.artist)];
+		}
+
+		return result;
+	}
+
+	private convertRawArtist(artist: MinimalArtist): ArtistCreditName {
+		return {
+			name: artist.name,
+			externalLink: new URL('https://www.deezer.com/artist/' + artist.id),
+		};
+	}
+
+	private determineAvailability(media: HarmonyMedium[]): string[] | undefined {
+		const trackAvailabilities = media.flatMap((medium) => medium.tracklist)
+			.map((track) => new Set(track.availableIn));
+
+		// calculate the intersection of all tracks' availabilities with Deezer's availability
+		return this.provider.availableRegions.filter((country) =>
+			trackAvailabilities.every((availability) => availability?.has(country))
+		);
 	}
 }
 
--- src/providers/abstract.ts
+++ src/providers/abstract.ts
@@ -155,7 +155,7 @@ export abstract class MetadataProvider<RawRelease> {
 		return this.supportedUrls.test(url);
 	}
 
-	protected generateMessage(text: string, type: MessageType = 'info'): ProviderMessage {
+	generateMessage(text: string, type: MessageType = 'info'): ProviderMessage {
 		return {
 			provider: this.name,
 			text,
@@ -221,6 +221,151 @@ export abstract class MetadataProvider<RawRelease> {
 		}
 
 		return response.json();
+	}
+}
+
+type AnyProvider = MetadataProvider<any>;
+
+type ExtractRelease<Provider extends AnyProvider> = Provider extends MetadataProvider<infer Release> ? Release: never;
+
+export abstract class ReleaseLookup<Provider extends AnyProvider, RawRelease = ExtractRelease<Provider>> {
+	constructor(
+		protected provider: Provider,
+		urlOrGtinOrId: URL | GTIN | string,
+		options: ReleaseOptions = {},
+	) {
+		// Use the provider's generally supported URLs if none have been specified for releases.
+		this.supportedUrls ??= provider.supportedUrls;
+	}
+
+	/**
+	 * URL pattern used to check supported domains, match release URLs and extract the ID from the URL.
+	 * The pathname has to contain a named group `id`, e.g. `/release/:id`.
+	 * Optionally the pathname can also contain a named group `region` which will be used to extract the preferred region.
+	 */
+	protected supportedUrls: URLPattern;
+
+	/** Constructs a canonical release URL for the given provider ID (and optional region). */
+	abstract constructReleaseUrl(id: string, region?: CountryCode): URL;
+
+	/** Constructs an optional API URL for a release using the given data. */
+	abstract constructReleaseApiUrl(options: RawReleaseOptions): URL | undefined;
+
+	/** Looks up the release which is identified by the given URL, GTIN/barcode or provider ID. */
+	getRelease(urlOrGtinOrId: URL | GTIN | string, options: ReleaseOptions = {}): Promise<HarmonyRelease> {
+		if (urlOrGtinOrId instanceof URL) {
+			const id = this.extractReleaseId(urlOrGtinOrId);
+			if (id === undefined) {
+				throw new ProviderError(this.provider.name, `Could not extract ID from ${urlOrGtinOrId}`);
+			}
+
+			// prefer region of the given release URL over the standard preferences
+			const region = this.extractReleaseRegion(urlOrGtinOrId);
+			if (region) {
+				// create a deep copy, we don't want to manipulate the caller's options
+				options = { ...options };
+				options.regions = [region];
+			}
+
+			return this.getReleaseById(id, options);
+		} else if (typeof urlOrGtinOrId === 'string' && !/^\d{12,14}$/.test(urlOrGtinOrId)) {
+			return this.getReleaseById(urlOrGtinOrId, options);
+		} else { // number or string with 12 to 14 digits, most likely a GTIN
+			return this.getReleaseByGTIN(urlOrGtinOrId, options);
+		}
+	}
+
+	/** Looks up the release which is identified by the given provider ID. */
+	getReleaseById(id: string, options: ReleaseOptions = {}): Promise<HarmonyRelease> {
+		const rawOptions = options as RawReleaseOptions;
+		rawOptions.lookup = { method: 'id', value: id };
+
+		return this.processReleaseLookup(rawOptions);
+	}
+
+	/** Looks up the release which is identified by the given GTIN/barcode. */
+	getReleaseByGTIN(gtin: GTIN, options: ReleaseOptions = {}): Promise<HarmonyRelease> {
+		const rawOptions = options as RawReleaseOptions;
+		rawOptions.lookup = { method: 'gtin', value: gtin.toString() };
+
+		return this.processReleaseLookup(rawOptions);
+	}
+
+	private async processReleaseLookup(rawOptions: RawReleaseOptions): Promise<HarmonyRelease> {
+		const startTime = performance.now();
+
+		const rawRelease = await this.getRawRelease(rawOptions);
+		const release = await this.convertRawRelease(rawRelease, rawOptions);
+
+		// store the elapsed time for each provider info record (just in case), although there should be only one
+		const elapsedTime = performance.now() - startTime;
+		release.info.providers.forEach((providerInfo) => providerInfo.processingTime = elapsedTime);
+
+		return this.withExcludedRegions(release);
+	}
+
+	/**
+	 * Loads the raw release data for the given lookup options.
+	 * This method is only used internally and guaranteed to be called with either a GTIN or a provider ID.
+	 */
+	protected abstract getRawRelease(options: RawReleaseOptions): Promise<RawResult<RawRelease>>;
+
+	/** Converts the given provider-specific raw release metadata into a common representation. */
+	protected abstract convertRawRelease(
+		rawResult: RawResult<RawRelease>,
+		options: RawReleaseOptions,
+	): MaybePromise<HarmonyRelease>;
+
+	/** Extracts the ID from a release URL. */
+	extractReleaseId(url: URL): string | undefined {
+		return this.supportedUrls.exec(url)?.pathname.groups.id;
+	}
+
+	/** Extracts the region from a release URL (if present). */
+	extractReleaseRegion(url: URL): CountryCode | undefined {
+		// do not return an empty string in case the group was declared as optional and is missing from the result
+		return this.supportedUrls.exec(url)?.pathname.groups.region || undefined;
+	}
+
+	/** Checks whether the provider supports the given URL for releases. */
+	supportsReleaseUrl(url: URL): boolean {
+		return this.supportedUrls.test(url);
+	}
+
+	protected generateReleaseInfo({ id, lookupInfo, messages = [], options }: {
+		id: string;
+		lookupInfo: ReleaseLookupInfo;
+		messages?: ProviderMessage[];
+		options: RawReleaseOptions;
+	}): ReleaseInfo {
+		// overwrite optional property with the actually used region (in order to build the accurate API URL)
+		options.lookup.region = lookupInfo.region;
+
+		return {
+			providers: [{
+				name: this.provider.name,
+				id,
+				region: lookupInfo.region,
+				url: this.constructReleaseUrl(id, lookupInfo.region),
+				apiUrl: this.constructReleaseApiUrl(options),
+			}],
+			messages,
+		};
+	}
+
+	/** Determines excluded regions of the release (if available regions have been specified for the provider). */
+	private withExcludedRegions(release: HarmonyRelease): HarmonyRelease {
+		const availableRegions = this.provider.availableRegions;
+		if (availableRegions.length && release.availableIn) {
+			if (release.availableIn.length) {
+				const releaseAvailability = new Set(release.availableIn);
+				release.excludedFrom = availableRegions.filter((region) => !releaseAvailability.has(region));
+			} else {
+				release.excludedFrom = [...availableRegions];
+			}
+		}
+
+		return release;
 	}
 }
 
