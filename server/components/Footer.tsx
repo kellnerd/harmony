c36d129ccbc1381f4fca2038b9013d901616c324
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-04-11 13:24:17 +0200

    feat(web): Add support URL to footer

--- server/components/Footer.tsx
+++ server/components/Footer.tsx
@@ -1,11 +1,13 @@
-import { codeUrl, revision } from '@/server/config.ts';
+import { codeUrl, revision, supportUrl } from '@/server/config.ts';
 
 export default function Footer() {
 	return (
 		<footer class='center'>
 			<p>
 				<small>
-					&copy; 2023-2024 David Kellner &bull; <a href={codeUrl}>Source</a> &bull; Revision
+					&copy; 2023-2024 David Kellner &bull; <a href={codeUrl}>Source</a> &bull; <a href={supportUrl}>Support</a>
+					{' '}
+					&bull; Revision
 				</small>{' '}
 				<code>{revision ?? 'unknown'}</code>
 			</p>
--- server/config.ts
+++ server/config.ts
@@ -1,5 +1,8 @@
 /** Source code URL. */
-export const codeUrl = Deno.env.get('HARMONY_CODE_URL') || 'https://github.com/kellnerd/harmony';
+export const codeUrl = Deno.env.get('HARMONY_CODE_URL') || 'https://github.com/kellnerd/harmony/';
+
+/** User support URL. */
+export const supportUrl = Deno.env.get('HARMONY_SUPPORT_URL') || new URL('issues', codeUrl).href;
 
 /** Current git revision. */
 export const revision = Deno.env.get('DENO_DEPLOYMENT_ID');
