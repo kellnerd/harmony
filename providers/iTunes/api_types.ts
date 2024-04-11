9354ffb8e848c0d65f24b8050574a3e44449b040
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-04-11 12:33:02 +0200

    refactor: Extract API types into their own modules

---
+++ providers/iTunes/api_types.ts
@@ -0,0 +1,96 @@
+import type { CountryCode } from '../../harmonizer/types.ts';
+
+export type Artist = {
+	wrapperType: 'artist';
+	artistType: 'Artist';
+	artistName: string;
+	artistLinkUrl: string;
+	artistId: number;
+	amgArtistId: number;
+	primaryGenreName: string;
+	primaryGenreId: number;
+};
+
+export type Collection = {
+	wrapperType: 'collection';
+	collectionType: 'Album';
+	artistId: number;
+	collectionId: number;
+	amgArtistId: number;
+	artistName: string;
+	collectionName: string;
+	collectionCensoredName: string;
+	// Various artists have no URL
+	artistViewUrl?: string;
+	collectionViewUrl: string;
+	artworkUrl60: string;
+	artworkUrl100: string;
+	collectionPrice?: number;
+	collectionExplicitness: Explicitness;
+	contentAdvisoryRating?: 'Explicit';
+	trackCount: number;
+	copyright: string;
+	country: string;
+	currency: string;
+	releaseDate: string;
+	primaryGenreName: string;
+};
+
+export type Track = {
+	wrapperType: 'track';
+	kind: Kind;
+	artistId: number;
+	collectionId: number;
+	trackId: number;
+	artistName: string;
+	collectionName: string;
+	trackName: string;
+	collectionCensoredName: string;
+	trackCensoredName: string;
+	artistViewUrl: string;
+	collectionViewUrl: string;
+	trackViewUrl: string;
+	previewUrl: string;
+	artworkUrl30: string;
+	artworkUrl60: string;
+	artworkUrl100: string;
+	collectionPrice: number;
+	trackPrice: number;
+	releaseDate: string;
+	collectionExplicitness: Explicitness;
+	trackExplicitness: Explicitness;
+	discCount: number;
+	discNumber: number;
+	trackCount: number;
+	trackNumber: number;
+	trackTimeMillis: number;
+	country: string;
+	currency: string;
+	primaryGenreName: string;
+	isStreamable: boolean;
+};
+
+export type Explicitness = 'clean' | 'explicit' | 'notExplicit';
+
+export type Kind =
+	| 'album'
+	| 'artist'
+	| 'book'
+	| 'coached-audio'
+	| 'feature-movie'
+	| 'interactive-booklet'
+	| 'music-video'
+	| 'pdf podcast'
+	| 'podcast-episode'
+	| 'software-package'
+	| 'song'
+	| 'tv-episode';
+
+export type Result<T> = {
+	resultCount: number;
+	results: Array<T>;
+	/** Custom property to remember the successfully queried region of the API. */
+	region: CountryCode;
+};
+
+export type ReleaseResult = Result<Collection | Track>;
--- providers/iTunes/mod.ts
+++ providers/iTunes/mod.ts
@@ -4,6 +4,7 @@ import { ResponseError } from '../../utils/errors.ts';
 import { isEqualGTIN, isValidGTIN } from '../../utils/gtin.ts';
 import { pluralWithCount } from '../../utils/plural.ts';
 
+import type { Collection, ReleaseResult, Result, Track } from './api_types.ts';
 import type {
 	ArtistCreditName,
 	Artwork,
@@ -255,98 +256,3 @@ export function getSourceImage(url: string) {
 
 	return imageUrl;
 }
-
-export type Result<T> = {
-	resultCount: number;
-	results: Array<T>;
-	/** Custom property to remember the successfully queried region of the API. */
-	region: CountryCode;
-};
-
-type ReleaseResult = Result<Collection | Track>;
-
-export type Artist = {
-	wrapperType: 'artist';
-	artistType: 'Artist';
-	artistName: string;
-	artistLinkUrl: string;
-	artistId: number;
-	amgArtistId: number;
-	primaryGenreName: string;
-	primaryGenreId: number;
-};
-
-export type Collection = {
-	wrapperType: 'collection';
-	collectionType: 'Album';
-	artistId: number;
-	collectionId: number;
-	amgArtistId: number;
-	artistName: string;
-	collectionName: string;
-	collectionCensoredName: string;
-	// Various artists have no URL
-	artistViewUrl?: string;
-	collectionViewUrl: string;
-	artworkUrl60: string;
-	artworkUrl100: string;
-	collectionPrice?: number;
-	collectionExplicitness: Explicitness;
-	contentAdvisoryRating?: 'Explicit';
-	trackCount: number;
-	copyright: string;
-	country: string;
-	currency: string;
-	releaseDate: string;
-	primaryGenreName: string;
-};
-
-export type Track = {
-	wrapperType: 'track';
-	kind: Kind;
-	artistId: number;
-	collectionId: number;
-	trackId: number;
-	artistName: string;
-	collectionName: string;
-	trackName: string;
-	collectionCensoredName: string;
-	trackCensoredName: string;
-	artistViewUrl: string;
-	collectionViewUrl: string;
-	trackViewUrl: string;
-	previewUrl: string;
-	artworkUrl30: string;
-	artworkUrl60: string;
-	artworkUrl100: string;
-	collectionPrice: number;
-	trackPrice: number;
-	releaseDate: string;
-	collectionExplicitness: Explicitness;
-	trackExplicitness: Explicitness;
-	discCount: number;
-	discNumber: number;
-	trackCount: number;
-	trackNumber: number;
-	trackTimeMillis: number;
-	country: string;
-	currency: string;
-	primaryGenreName: string;
-	isStreamable: boolean;
-};
-
-type Explicitness = 'clean' | 'explicit' | 'notExplicit';
-
-type Kind =
-	| 'album'
-	| 'artist'
-	| 'book'
-	| 'coached-audio'
-	| 'feature-movie'
-	| 'interactive-booklet'
-	| 'music-video'
-	| 'pdf podcast'
-	| 'podcast-episode'
-	| 'software-package'
-	| 'song'
-	| 'tv-episode';
