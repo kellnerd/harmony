c7dcf562fa1b6db02fac60951938ad69699b970d
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-04-11 17:08:08 +0200

    chore(deps): Update to Fresh 1.6.8

--- server/fresh.gen.ts
+++ server/fresh.gen.ts
@@ -2,21 +2,22 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from './routes/_app.tsx';
-import * as $1 from './routes/index.tsx';
-import * as $2 from './routes/release.tsx';
-import * as $$0 from './islands/RegionList.tsx';
+import * as $_app from './routes/_app.tsx';
+import * as $index from './routes/index.tsx';
+import * as $release from './routes/release.tsx';
+import * as $RegionList from './islands/RegionList.tsx';
+import { type Manifest } from '$fresh/server.ts';
 
 const manifest = {
 	routes: {
-		'./routes/_app.tsx': $0,
-		'./routes/index.tsx': $1,
-		'./routes/release.tsx': $2,
+		'./routes/_app.tsx': $_app,
+		'./routes/index.tsx': $index,
+		'./routes/release.tsx': $release,
 	},
 	islands: {
-		'./islands/RegionList.tsx': $$0,
+		'./islands/RegionList.tsx': $RegionList,
 	},
 	baseUrl: import.meta.url,
-};
+} satisfies Manifest;
 
 export default manifest;
