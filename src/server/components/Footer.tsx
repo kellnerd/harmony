7e54a284da0ef0d5211705622f8dc43a07745c2f
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-04-11 11:21:32 +0200

    feat(web): Link to source code in the footer

--- src/server/components/Footer.tsx
+++ src/server/components/Footer.tsx
@@ -1,10 +1,14 @@
 export default function Footer() {
 	const revision = Deno.env.get('DENO_DEPLOYMENT_ID');
+	const codeUrl = Deno.env.get('HARMONY_CODE_URL') || 'https://github.com/kellnerd/harmony';
 
 	return (
 		<footer class='center'>
 			<p>
-				<small>Copyright &copy; 2023 David Kellner &bull; Revision</small> <code>{revision ?? 'unknown'}</code>
+				<small>
+					&copy; 2023-2024 David Kellner &bull; <a href={codeUrl}>Source</a> &bull; Revision
+				</small>{' '}
+				<code>{revision ?? 'unknown'}</code>
 			</p>
 		</footer>
 	);
