813ee1e325a1c8facaa00ce41b67badebf1d565d
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-04-12 19:12:37 +0200

    feat(web): Use GTIN instead of URL in permalinks whenever possible
    Also include region and all enabled providers which were looked up by
    GTIN, although these are still ignored by the current route handler.
    This approach should result in shorter permalinks on average.

--- server/state.ts
+++ server/state.ts
@@ -2,10 +2,25 @@ import type { ReleaseInfo } from '@/harmonizer/types.ts';
 import { isDefined } from '@/utils/predicate.ts';
 
 export function createReleasePermalink(info: ReleaseInfo, baseUrl: URL): URL {
-	const providerUrls = info.providers.map((providerInfo) => providerInfo.url.href);
-	const cacheTimestamps = info.providers.map((providerInfo) => providerInfo.cacheTime).filter(isDefined);
+	const providersLookedUpByGtin = info.providers.filter((provider) => provider.lookup.method === 'gtin');
+	const providersLookedUpByUrl = info.providers.filter((provider) => provider.lookup.method === 'id');
+	const usedRegion = info.providers.map((provider) => provider.lookup.region).find(isDefined);
+	const cacheTimestamps = info.providers.map((provider) => provider.cacheTime).filter(isDefined);
 
-	const state = new URLSearchParams(providerUrls.map((url) => ['url', url]));
+	// Add provider URLs for all providers which were looked up by URL.
+	// TODO: Support lookup by `provider=id` pairs to get shorter permalinks.
+	const state = new URLSearchParams(providersLookedUpByUrl.map((provider) => ['url', provider.url.href]));
+	if (providersLookedUpByGtin.length) {
+		state.append('gtin', providersLookedUpByGtin[0].lookup.value);
+		// Add all enabled providers which were looked up by GTIN (with empty provider ID value).
+		for (const provider of providersLookedUpByGtin) {
+			state.append(provider.name, '');
+		}
+	}
+	// If a region has been used for lookup, it should be the same for all providers.
+	if (usedRegion) {
+		state.append('region', usedRegion);
+	}
 	// Maximum timestamp can be used to load the latest snapshot up to this timestamp for each provider.
 	state.append('ts', Math.max(...cacheTimestamps).toFixed(0));
 
