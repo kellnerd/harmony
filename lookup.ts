07d80b14d3d1aa3d4c5c4b345776243855b80474
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-04-14 23:47:27 +0200

    refactor(lookup): Make provider preferences optional
    They are also optional for the underlying merge function.

--- lookup.ts
+++ lookup.ts
@@ -211,7 +211,7 @@ export class CombinedReleaseLookup {
 	}
 
 	/** Ensures that all requested providers have been looked up and returns the combined release. */
-	async getMergedRelease(providerPreferences: ProviderPreferences): Promise<HarmonyRelease> {
+	async getMergedRelease(providerPreferences?: ProviderPreferences): Promise<HarmonyRelease> {
 		const releaseMap = await this.getCompleteProviderReleaseMapping();
 		const release = mergeRelease(releaseMap, providerPreferences);
 		// Prepend error and warning messages of the combined lookup.
