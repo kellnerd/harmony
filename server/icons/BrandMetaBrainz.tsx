c948ede12c228b5fddca53827c494a2622b9cdda
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-04-17 21:21:18 +0200

    style(web): Create MetaBrainz SVG icon (Tabler style)
    SVG is a monochrome outline version, TSX also exists as filled version.
    TSX is monochrome by default, but if a color parameter is given, it will
    be applied to the left half only, the right half will be orange then.

---
+++ server/icons/BrandMetaBrainz.tsx
@@ -0,0 +1,27 @@
+export default function IconBrandMetaBrainz({
+	size = 24,
+	color = 'currentColor',
+	stroke = 2,
+	...props
+}) {
+	const brainzColor = color === 'currentColor' ? undefined : '#eb743b';
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			class='icon icon-tabler icon-tabler-brand-metabrainz'
+			width={size}
+			height={size}
+			viewBox='0 0 24 24'
+			stroke-width={stroke}
+			stroke={color}
+			fill='none'
+			stroke-linecap='round'
+			stroke-linejoin='round'
+			{...props}
+		>
+			<path stroke='none' d='M0 0h24v24H0z' fill='none' />
+			<path d='M2.5 6.5v11l8 4.6V1.9z' />
+			<path stroke={brainzColor} d='M21.5 6.5v11l-8 4.6V1.9z' />
+		</svg>
+	);
+}
