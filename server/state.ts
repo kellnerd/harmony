kellnerd/harmony

commit 4876be2ada1af12f5d6e06f2d794daf51934923e

fix(web): Fail lookup for an invalid timestamp

server/state.ts
@@ -4,6 +4,7 @@ import { allProviderSimpleNames } from '@/providers/mod.ts';
 import { ensureValidGTIN } from '@/utils/gtin.ts';
 import { isDefined, isNotEmpty } from '@/utils/predicate.ts';
 import { assertCountryCode } from '@/utils/regions.ts';
+import { assertTimestamp } from '@/utils/time.ts';
 import { simplifyName } from 'utils/string/simplify.js';
 
 /**
@@ -81,8 +82,8 @@ export function extractReleaseLookupState(lookupUrl: URL): ReleaseLookupParamete
 		}
 	}
 
-	const ts = searchParams.get('ts') ?? '';
-	const snapshotMaxTimestamp = ts !== '' ? parseInt(ts) : undefined;
+	const snapshotMaxTimestamp = parseInt(searchParams.get('ts') ?? '');
+	assertTimestamp(snapshotMaxTimestamp);
 
 	return {
 		providers: requestedProviders.size ? requestedProviders : undefined,

utils/time.ts
@@ -1,3 +1,5 @@
+import { assert } from 'std/testing/asserts.ts';
+
 export function formatDuration(ms: number, {
 	showMs = false,
 } = {}): string {
@@ -25,7 +27,7 @@ export function formatDuration(ms: number, {
 
 	ms %= 1000;
 	if (ms || showMs) {
-		duration += '.' + ms.toString().padStart(3, '0');
+		duration += '.' + ms.toFixed(0).padStart(3, '0');
 	}
 
 	return duration;
@@ -33,4 +35,9 @@ export function formatDuration(ms: number, {
 
 export function formatTimestampAsISOString(unixSeconds: number): string {
 	return new Date(unixSeconds * 1000).toISOString().replace('.000', '');
+}
+
+/** Asserts that the given number is a valid timestamp. */
+export function assertTimestamp(ts: number) {
+	assert(Number.isSafeInteger(ts) && ts >= 0, 'Timestamp has to be a non-negative integer');
 }

commit e8463659e9b106032a0e6430627ebbfc803f4639

fix(web): Do not parse and validate empty/missing timestamp
Regression from 4876be2ada1af12f5d6e06f2d794daf51934923e

server/state.ts
@@ -82,8 +82,12 @@ export function extractReleaseLookupState(lookupUrl: URL): ReleaseLookupParamete
 		}
 	}
 
-	const snapshotMaxTimestamp = parseInt(searchParams.get('ts') ?? '');
-	assertTimestamp(snapshotMaxTimestamp);
+	const ts = searchParams.get('ts') ?? '';
+	let snapshotMaxTimestamp: number | undefined = undefined;
+	if (ts) {
+		snapshotMaxTimestamp = parseInt(ts);
+		assertTimestamp(snapshotMaxTimestamp);
+	}
 
 	return {
 		providers: requestedProviders.size ? requestedProviders : undefined,