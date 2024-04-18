a5864eaef1218255bbde6124abba3178aa153638
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-04-18 17:19:00 +0200

    feat(web): Link SVG and PNG favicons in the HTML head
    `favicon.ico` is an old legacy format which is better left unlinked to
    let modern browsers choose a better format, old browsers which require
    the format will still autodetect it in the server's root folder.

--- server/routes/_app.tsx
+++ server/routes/_app.tsx
@@ -7,7 +7,10 @@ export default function App({ Component }: PageProps) {
 				<meta charset='utf-8' />
 				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
 				<title>Harmony</title>
-				<link rel='stylesheet' href='harmony.css' />
+				<link rel='stylesheet' href='/harmony.css' />
+				<link rel='icon' type='image/svg+xml' href='/favicon.svg' />
+				<link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+				<link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
 			</head>
 			<body>
 				<Component />
