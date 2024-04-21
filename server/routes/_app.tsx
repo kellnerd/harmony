55edb4351d6c665d09b87afde3ca9891bdb6dddc
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-04-21 17:53:27 +0200

    fix(web): Prevent Chrome from loading favicon.ico instead of the SVG
    Following version 4.0 of a thorough guide with lots of sources:
    https://dev.to/masakudamatsu/favicon-nightmare-how-to-maintain-sanity-3al7
    Only omitting the Apple Touch icon link as per my previous reasoning.
    
    Both Chrome and Firefox are now loading the SVG icon for bookmarks.
    
    Drop PNG favicons, replace 64x64 icon in ICO file with more common 48x48.

--- server/routes/_app.tsx
+++ server/routes/_app.tsx
@@ -8,9 +8,8 @@ export default function App({ Component }: PageProps) {
 				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
 				<title>Harmony</title>
 				<link rel='stylesheet' href='/harmony.css' />
-				<link rel='icon' type='image/svg+xml' href='/favicon.svg' />
-				<link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
-				<link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+				<link rel='icon' href='/favicon.ico' sizes='48x48' />
+				<link rel='icon' href='/favicon.svg' sizes='any' type='image/svg+xml' />
 				<link rel='manifest' href='/site.webmanifest' />
 			</head>
 			<body>
