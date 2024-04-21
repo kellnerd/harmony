3ea2224cbdbbd98352a860c612074e5149f85c51
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-04-21 21:36:14 +0200

    style(web): Rework footer to an inline list, shorten revision hash
    Remove copyright, I want to encourage others to contribute and it is a
    chore having to update the copyright years in multiple places.

--- server/components/Footer.tsx
+++ server/components/Footer.tsx
@@ -3,13 +3,17 @@ import { codeUrl, revision, supportUrl } from '@/server/config.ts';
 export default function Footer() {
 	return (
 		<footer class='center'>
-			<p>
-				<small>
-					&copy; 2023-2024 David Kellner &bull; <a href={codeUrl.href}>Source</a> &bull;{' '}
-					<a href={supportUrl.href}>Support</a> &bull; Revision
-				</small>{' '}
-				<code>{revision ?? 'unknown'}</code>
-			</p>
+			<ul class='inline'>
+				<li>
+					Revision <code>{revision?.substring(0, 7) ?? 'unknown'}</code>
+				</li>
+				<li>
+					<a href={codeUrl.href}>Source</a>
+				</li>
+				<li>
+					<a href={supportUrl.href}>Support</a>
+				</li>
+			</ul>
 		</footer>
 	);
 }
