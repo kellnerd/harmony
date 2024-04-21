9b0c608de8cfb70591b5e8da66b9e6b9eee2fa32
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-04-21 16:35:46 +0200

    feat(web): Add web application manifest which defines icons and colors
    These are used when adding a website to the home screen or installing it
    as a Progressive Web App (on Android).
    
    Generate PNG icons in 192x192 and 512x512, these seem to be the relevant
    sizes nowadays (Chrome, Firefox), SVG is only picked by Chrome so far.
    
    Background is using the dark brand color, theme the red brand color.

--- server/routes/_app.tsx
+++ server/routes/_app.tsx
@@ -11,6 +11,7 @@ export default function App({ Component }: PageProps) {
 				<link rel='icon' type='image/svg+xml' href='/favicon.svg' />
 				<link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
 				<link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+				<link rel='manifest' href='/site.webmanifest' />
 			</head>
 			<body>
 				<Component />
