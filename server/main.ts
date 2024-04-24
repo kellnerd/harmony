ec22c8ca5183edd895028c7313e670865d50c5e6
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-04-25 01:06:18 +0200

    fix(web): Close Fresh server on SIGINT or SIGTERM signal

--- server/main.ts
+++ server/main.ts
@@ -1,7 +1,25 @@
-// automatically load .env environment variable file (before everything else)
+// Automatically load .env environment variable file (before everything else).
 import 'std/dotenv/load.ts';
 
 import manifest from './fresh.gen.ts';
 import { start } from 'fresh/server.ts';
 
-await start(manifest);
+const abortController = new AbortController();
+const isWindows = Deno.build.os === 'windows';
+
+for (const signal of ['SIGINT', 'SIGTERM'] as const) {
+	// On Windows only "SIGINT" (CTRL+C) and "SIGBREAK" (CTRL+Break) are supported.
+	if (isWindows && signal !== 'SIGINT') break;
+
+	Deno.addSignalListener(signal, () => {
+		console.info(`App received '${signal}', aborting...`);
+		abortController.abort(signal);
+	});
+}
+
+await start(manifest, { server: { signal: abortController.signal } });
+
+console.info('Fresh server and all its connections were closed.');
+
+// Explicitly exit, otherwise dev servers would restart on file changes.
+Deno.exit();
