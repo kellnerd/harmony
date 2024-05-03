4fb5eb005faba9bc646c5631033afff23a6287f5
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-05-03 11:15:49 +0200

    perf(mb): Also cache unresolved external IDs within a release context
    This is necessary to avoid multiple unsuccessful API requests for the
    same entity (e.g. an artist on multiple tracks) within a short interval.

--- musicbrainz/mbid_mapping.ts
+++ musicbrainz/mbid_mapping.ts
@@ -9,40 +9,57 @@ import { constructEntityUrl } from '@/providers/mod.ts';
  *
  * Tries to lookup each of the given external IDs until an MBID is found.
  * Before sending requests to the MusicBrainz API, the MBID cache is checked.
+ *
+ * Accepts an optional context cache which also stores unresolved external IDs.
+ * This is useful to avoid multiple unsuccessful API requests for the same entity within a short interval.
  */
 export async function resolveToMbid(
 	entityIds: ExternalEntityId[],
 	entityType: RelatableEntityType,
+	contextCache?: Record<string, string>,
 ): Promise<string | undefined> {
 	if (!entityIds.length) return;
 
-	// First check the cache for each entity ID.
+	// First check the caches for each entity ID.
 	for (const entityId of entityIds) {
-		const mbid = getCachedMbid(entityId);
+		const mbid = getCachedMbid(entityId, contextCache);
 		if (mbid) {
 			return mbid;
+		} else if (mbid === '') {
+			// Empty MBID is used by the context cache to indicate that further requests should be skipped.
+			return;
 		}
 	}
 
 	// If the MBID is not cached, try to lookup canonical entity URLs with the MB API.
 	for (const entityId of entityIds) {
-		const externalLink = constructEntityUrl(entityId);
+		const externalUrl = constructEntityUrl(entityId);
 		try {
-			const result = await MB.browseUrl(externalLink, {
+			const result = await MB.browseUrl(externalUrl, {
 				inc: [`${entityType}-rels`],
 			});
 			const rel = result.relations.find((rel) => rel['target-type'] === entityType);
-			if (!rel) continue;
+			if (!rel) {
+				if (contextCache) {
+					// Only writes to the context cache to indicate that further requests for this URL should be skipped.
+					setCachedMbid(entityId, '', contextCache);
+				}
+				continue;
+			}
 
 			// @ts-ignore: `entityType` is not narrowed, but every specific value is a valid key here.
 			const targetEntity = rel[entityType] as EntityWithMbid;
 			const mbid = targetEntity.id;
-			console.debug('Resolved', entityType, mbid, externalLink.href);
-			setCachedMbid(entityId, mbid);
+			console.debug('Resolved', entityType, mbid, externalUrl.href);
+			setCachedMbid(entityId, mbid, contextCache);
 
 			return mbid;
 		} catch (error) {
-			if (error instanceof ApiError) continue;
+			if (error instanceof ApiError) {
+				// Only writes to the context cache to indicate that further requests for this URL should be skipped.
+				setCachedMbid(entityId, '', contextCache);
+				continue;
+			}
 			throw error;
 		}
 	}
@@ -51,30 +68,39 @@ export async function resolveToMbid(
 /** Resolves all external links for artists and labels of the given release to their MBIDs. */
 export async function resolveReleaseMbids(release: HarmonyRelease) {
 	const { artists, labels, media } = release;
+	const contextCache = {};
 
-	await resolveMbidsForMultipleEntities(artists, 'artist');
+	await resolveMbidsForMultipleEntities(artists, 'artist', contextCache);
 	if (labels) {
-		await resolveMbidsForMultipleEntities(labels, 'label');
+		await resolveMbidsForMultipleEntities(labels, 'label', contextCache);
 	}
 	for (const medium of media) {
 		for (const track of medium.tracklist) {
 			if (track.artists) {
-				await resolveMbidsForMultipleEntities(track.artists, 'artist');
+				await resolveMbidsForMultipleEntities(track.artists, 'artist', contextCache);
 			}
 		}
 	}
 }
 
-async function resolveMbidForEntity(entity: ResolvableEntity, entityType: RelatableEntityType) {
+async function resolveMbidForEntity(
+	entity: ResolvableEntity,
+	entityType: RelatableEntityType,
+	contextCache?: Record<string, string>,
+) {
 	if (entity.mbid) return;
 	if (entity.externalIds) {
-		entity.mbid = await resolveToMbid(entity.externalIds, entityType);
+		entity.mbid = await resolveToMbid(entity.externalIds, entityType, contextCache);
 	}
 }
 
-function resolveMbidsForMultipleEntities(entities: ResolvableEntity[], entityType: RelatableEntityType) {
+function resolveMbidsForMultipleEntities(
+	entities: ResolvableEntity[],
+	entityType: RelatableEntityType,
+	contextCache?: Record<string, string>,
+) {
 	return Promise.all(
-		entities.map((entity) => resolveMbidForEntity(entity, entityType)),
+		entities.map((entity) => resolveMbidForEntity(entity, entityType, contextCache)),
 	);
 }
 
@@ -83,19 +109,24 @@ const cache = sessionStorage;
 const cacheKeySeparator = ':';
 const mbidCachePrefix = 'mbid';
 
-function getCachedMbid(entityId: ExternalEntityId): string | undefined {
-	const key = [mbidCachePrefix, entityId.provider, entityId.type, entityId.id];
-	return cache.getItem(key.join(cacheKeySeparator)) ?? undefined;
+function getCachedMbid(entityId: ExternalEntityId, contextCache?: Record<string, string>): string | undefined {
+	const key = [mbidCachePrefix, entityId.provider, entityId.type, entityId.id].join(cacheKeySeparator);
+	return contextCache?.[key] ?? cache.getItem(key) ?? undefined;
 }
 
-function setCachedMbid(entityId: ExternalEntityId, mbid: string) {
-	const key = [mbidCachePrefix, entityId.provider, entityId.type, entityId.id];
-	setCacheItem(key, mbid);
+function setCachedMbid(entityId: ExternalEntityId, mbid: string, contextCache?: Record<string, string>) {
+	const key = [mbidCachePrefix, entityId.provider, entityId.type, entityId.id].join(cacheKeySeparator);
+	if (contextCache) {
+		contextCache[key] = mbid;
+	}
+	if (mbid !== '') {
+		setCacheItem(key, mbid);
+	}
 }
 
-function setCacheItem(key: string[], value: string, retries = 1) {
+function setCacheItem(key: string, value: string, retries = 1) {
 	try {
-		cache.setItem(key.join(cacheKeySeparator), value);
+		cache.setItem(key, value);
 	} catch (error) {
 		console.debug('Failed to cache item:', error);
 		if (retries > 0) {
@@ -103,7 +134,7 @@ function setCacheItem(key: string[], value: string, retries = 1) {
 			deleteRandomCacheItem();
 			setCacheItem(key, value, retries - 1);
 		} else {
-			console.warn(`Caching of '${key.join(cacheKeySeparator)}' failed repeatedly`);
+			console.warn(`Caching of '${key}' failed repeatedly`);
 		}
 	}
 }
