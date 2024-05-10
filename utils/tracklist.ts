f44de6dc570352b9839554b1cd847fff98e056bb
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-05-10 18:39:51 +0200

    fix(util): Count releases without media as "0 tracks"

--- utils/tracklist.ts
+++ utils/tracklist.ts
@@ -4,6 +4,10 @@ import { plural } from '@/utils/plural.ts';
 /** Creates a per medium track count summary for the given release. */
 export function trackCountSummary(release: HarmonyRelease): string {
 	const trackCounts = release.media.map((medium) => medium.tracklist.length);
+	if (!trackCounts.length) {
+		// Release with media has 0 tracks.
+		trackCounts.push(0);
+	}
 	const totalTrackCount = trackCounts.reduce((sum, current) => sum + current, 0);
 
 	return `${trackCounts.join('+')} ${plural(totalTrackCount, 'track')}`;
