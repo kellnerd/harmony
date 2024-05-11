466ac32976bf230be3e0ddc7691f88419b001c4a
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-05-11 23:51:00 +0200

    feat(web): Fill info box with basic usage instructions

--- server/routes/index.tsx
+++ server/routes/index.tsx
@@ -1,8 +1,6 @@
 import { MessageBox } from '@/server/components/MessageBox.tsx';
 import ReleaseLookup from '@/server/components/ReleaseLookup.tsx';
 
-import { providers } from '@/providers/mod.ts';
-
 export default function Home() {
 	return (
 		<>
@@ -14,7 +12,16 @@ export default function Home() {
 				<p class='subtitle'>Music Metadata Aggregator and MusicBrainz Importer</p>
 			</header>
 			<main>
-				<MessageBox message={{ text: `Supported providers: ${providers.displayNames.join(', ')}`, type: 'info' }} />
+				<MessageBox
+					message={{
+						type: 'info',
+						text: `
+1. Enter a release URL for one of the supported providers and/or a barcode.
+2. Select (additional) providers which should be looked up by barcode.
+3. Optionally specify a country code (two letters) for providers with region-specific pages.
+						`,
+					}}
+				/>
 				<h2 class='center'>Release Lookup</h2>
 				<ReleaseLookup formAction='release' />
 			</main>
