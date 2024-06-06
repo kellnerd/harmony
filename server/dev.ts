926c2196ae307e55d30a348b359503b9407c6738
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-06-06 17:39:21 +0200

    fix(web): Load `.env` file also for development servers

--- server/dev.ts
+++ server/dev.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
 
+// Automatically load .env environment variable file and configure logger (before anything else).
+import 'std/dotenv/load.ts';
+
 import dev from 'fresh/dev.ts';
 
 await dev(import.meta.url, './main.ts');
