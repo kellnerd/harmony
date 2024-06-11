b96a4c05096cedc4e4690fbec509dbd96b04e1ff
Philipp Wolfer <ph.wolfer@gmail.com>
2024-06-11 07:01:21 +0200

    fix(utils): fix descriptions for label tests

--- utils/label.test.ts
+++ utils/label.test.ts
@@ -5,18 +5,18 @@ import { describe, it } from 'std/testing/bdd.ts';
 
 import type { FunctionSpec } from './test_spec.ts';
 
-describe('GTIN validator', () => {
+describe('split labels', () => {
 	const passingCases: FunctionSpec<typeof splitLabels> = [
 		['preserves single label', 'Nuclear Blast', [{ name: 'Nuclear Blast' }]],
-		['split multiple labels', 'Roc Nation/RocAFella/IDJ', [{ name: 'Roc Nation' }, { name: 'RocAFella' }, {
+		['splits multiple labels', 'Roc Nation/RocAFella/IDJ', [{ name: 'Roc Nation' }, { name: 'RocAFella' }, {
 			name: 'IDJ',
 		}]],
-		['split multiple labels, trim values', 'Roc Nation / RocAFella / IDJ', [{ name: 'Roc Nation' }, {
+		['splits multiple labels and trims values', 'Roc Nation / RocAFella / IDJ', [{ name: 'Roc Nation' }, {
 			name: 'RocAFella',
 		}, {
 			name: 'IDJ',
 		}]],
-		['preserve label whose suffix contains a slash', 'EMI Belgium SA/NV', [{ name: 'EMI Belgium SA/NV' }]],
+		['preserves label whose suffix contains a slash', 'EMI Belgium SA/NV', [{ name: 'EMI Belgium SA/NV' }]],
 	];
 
 	passingCases.forEach(([description, input, expected]) => {
