5c8e23abfa62ebec99a498ffad381588d1d62394
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-10-18 23:05:41 +0200

    chore: Make code formatter happy

--- server/routes/release.tsx
+++ server/routes/release.tsx
@@ -50,7 +50,8 @@ export default defineRoute(async (req, ctx) => {
 			const mbInfo = release.info.providers.find((provider) => provider.name === 'MusicBrainz');
 			if (mbInfo) {
 				release.info.messages.push({
-					text: `Release with GTIN ${release.gtin} already exists on MusicBrainz ([show actions](release/actions?release_mbid=${mbInfo.id}))`,
+					text:
+						`Release with GTIN ${release.gtin} already exists on MusicBrainz ([show actions](release/actions?release_mbid=${mbInfo.id}))`,
 					type: 'info',
 				});
 			}
