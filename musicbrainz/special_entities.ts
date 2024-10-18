c865579e50e2c4d19d68dd8346c05ac68c1e6742
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-10-18 11:09:42 +0200

    chore(mb): Pre-define a few special artists and labels

---
+++ musicbrainz/special_entities.ts
@@ -0,0 +1,21 @@
+import { ArtistCreditName, Label } from '@/harmonizer/types.ts';
+
+export const noArtist: ArtistCreditName = {
+	name: '[no artist]',
+	mbid: 'eec63d3c-3b81-4ad4-b1e4-7c147d4d2b61',
+};
+
+export const unknownArtist: ArtistCreditName = {
+	name: '[unknown]',
+	mbid: '125ec42a-7229-4250-afc5-e057484327fe',
+};
+
+export const variousArtists: ArtistCreditName = {
+	name: 'Various Artists',
+	mbid: '89ad4ac3-39f7-470e-963a-56509c546377',
+};
+
+export const noLabel: Label = {
+	name: '[no label]',
+	mbid: '157afde4-4bf5-4039-8ad2-5a15acc85176',
+};
