bc889821fe921ae2ab820ba5d4924e7cb95c9871
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-12-07 17:34:17 +0100

    feat(web): Seed edit note for ISRC submission with MagicISRC

--- server/components/ISRCSubmission.tsx
+++ server/components/ISRCSubmission.tsx
@@ -1,17 +1,26 @@
-import { HarmonyRelease } from '@/harmonizer/types.ts';
+import type { HarmonyRelease } from '@/harmonizer/types.ts';
+import { musicbrainzBaseUrl } from '@/server/config.ts';
+import { join } from 'std/url/join.ts';
 
 export function MagicISRC({ release, targetMbid }: { release: HarmonyRelease; targetMbid?: string }) {
 	const allTracks = release.media.flatMap((medium) => medium.tracklist);
-	if (!allTracks.some((track) => track.isrc)) {
+	const isrcSource = release.info.sourceMap?.isrc;
+	const isrcProvider = release.info.providers.find((provider) => provider.name === isrcSource);
+	if (!(allTracks.some((track) => track.isrc) && isrcProvider)) {
 		return <></>;
 	}
 
 	const query = new URLSearchParams(
 		allTracks.map((track, index) => [`isrc${index + 1}`, track.isrc ?? '']),
 	);
+	let editNote = `Submit ISRCs from ${isrcProvider.url.href}`;
 	if (targetMbid) {
+		const releaseUrl = join(musicbrainzBaseUrl, 'release', targetMbid);
+		editNote += ` to ${releaseUrl.href}`;
 		query.set('musicbrainzid', targetMbid);
 	}
+	query.set('edit-note', editNote);
+
 	const submissionUrl = new URL('https://magicisrc.kepstin.ca');
 	submissionUrl.search = query.toString();
 
