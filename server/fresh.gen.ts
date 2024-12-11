6550ec9f672adcef8f804a61933a73174157638f
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-12-11 19:26:57 +0100

    feat(web): Add "about" page with table of all supported providers

--- server/fresh.gen.ts
+++ server/fresh.gen.ts
@@ -4,6 +4,7 @@
 
 import * as $_app from './routes/_app.tsx';
 import * as $_middleware from './routes/_middleware.ts';
+import * as $about from './routes/about.tsx';
 import * as $icon_sprite_svg from './routes/icon-sprite.svg.tsx';
 import * as $index from './routes/index.tsx';
 import * as $release from './routes/release.tsx';
@@ -16,6 +17,7 @@ const manifest = {
 	routes: {
 		'./routes/_app.tsx': $_app,
 		'./routes/_middleware.ts': $_middleware,
+		'./routes/about.tsx': $about,
 		'./routes/icon-sprite.svg.tsx': $icon_sprite_svg,
 		'./routes/index.tsx': $index,
 		'./routes/release.tsx': $release,
