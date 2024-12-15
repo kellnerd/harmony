e83178a9b400cd41e5918bd114725ccdf6b3e393
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-12-15 20:22:00 +0100

    style(web): Replace IFPI text icon by a Tabler-style icon
    It uses a fixed stroke width of 1px for the four letters, only the curve
    uses the standard dynamic stroke of usually 2px on the 24x24 grid.

--- server/icons/BrandIfpi.tsx
+++ server/icons/BrandIfpi.tsx
@@ -14,13 +14,19 @@ export default function IconBrandIfpi({
 			stroke-width={stroke}
 			stroke={color}
 			fill='none'
+			stroke-linecap='round'
+			stroke-linejoin='round'
 			{...props}
 		>
-			<path d='m2.5 4.5v14.2' />
-			<path d='m21.4 4.5v14.2' />
-			<path d='m11.2 5.6h-4.9v13.1' />
-			<path d='m6 11.7h4.5' />
-			<path d='m13.5 18.7v-13.1h3l1.1 1.1v4.5l-1.1 1.1h-3' />
+			<path d='m10.4 5.6c-0.5 2.6-3.9 13.1-8 10.5-3.7-2.3 6.7-10.3 20-9.7' />
+			<g stroke-width={1}>
+				<path d='m11.8 10.9v0.01' />
+				<path d='m11.5 12.7-0.5 3.8' />
+				<path d='m13.4 16.7 0.6-5.2c0.1-1 0.6-1.4 1.3-1.4m-0.5 2.5h-1.7' />
+				<path d='m15.8 18.6 0.8-6c4-1.4 3.5 4.6-0.5 4' />
+				<path d='m21.8 10.8v0.01' />
+				<path d='m21.5 12.6-0.5 3.8' />
+			</g>
 		</svg>
 	);
 }
