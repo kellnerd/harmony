cd9be8e8d0ea57297567f1e0b155a444a9d09e23
reubot <reuben_p@yahoo.com>
2024-12-16 02:26:14 +1000

    feat(web): Add IFPI and MusicBrainz ISRC search links
    * feat(web): Add MusicBrainz isrc link
    
    * style(web): Add IFPI brand icon
    
    * feat(web): Add IFPI ISRC search links
    
    use the same size as LinkedEntity
    use butt edges for IFPI logo
    MusicBrainz icon on the right

---
+++ server/icons/BrandIfpi.tsx
@@ -0,0 +1,28 @@
+export default function IconBrandIfpi({
+	size = 24,
+	color = 'currentColor',
+	stroke = 2.,
+	...props
+}) {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			class='icon icon-tabler icon-tabler-brand-ifpi'
+			width={size}
+			height={size}
+			viewBox='0 0 24 24'
+			stroke-width={stroke}
+			stroke={color}
+			fill='none'
+			{...props}
+		>
+			<g transform='translate(-343.3 -311.3)' stroke-linecap='butt' stroke-linejoin='miter'>
+				<path d='m345.8 315.8v14.25' />
+				<path d='m364.7 315.8v14.25' />
+				<path d='m354.5 316.9h-4.875v13.12' />
+				<path d='m349.3 323h4.5' />
+				<path d='m356.8 330v-13.12h3l1.125 1.125v4.5l-1.125 1.125h-3' />
+			</g>
+		</svg>
+	);
+}
