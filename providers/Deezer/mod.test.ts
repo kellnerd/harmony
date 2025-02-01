f49ebd1a08b5702d492b5b664f7e2715843fb94e
David Kellner <52860029+kellnerd@users.noreply.github.com>
2025-02-01 12:20:17 +0100

    test(Deezer): Expand URL spec to cover all supported kinds of URL

--- providers/Deezer/mod.test.ts
+++ providers/Deezer/mod.test.ts
@@ -10,9 +10,23 @@ describe('Deezer provider', () => {
 
 	describeProvider(deezer, {
 		urls: [{
+			description: 'release page',
+			url: new URL('https://www.deezer.com/album/11591214'),
+			id: { type: 'album', id: '11591214' },
+			isCanonical: true,
+		}, {
 			description: 'localized release page',
-			url: new URL('https://www.deezer.com/en/album/629506181'),
+			url: new URL('https://www.deezer.com/fr/album/629506181'),
 			id: { type: 'album', id: '629506181' },
+		}, {
+			description: 'localized release page without www subdomain',
+			url: new URL('https://deezer.com/en/album/521038732'),
+			id: { type: 'album', id: '521038732' },
+		}, {
+			description: 'artist page',
+			url: new URL('https://www.deezer.com/artist/8686'),
+			id: { type: 'artist', id: '8686' },
+			isCanonical: true,
 		}, {
 			description: 'playlist page',
 			url: new URL('https://www.deezer.com/en/playlist/1976454162'),
