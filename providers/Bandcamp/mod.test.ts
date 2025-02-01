f8eec1607d197e5724ded534c119d41c02b413d8
David Kellner <52860029+kellnerd@users.noreply.github.com>
2025-02-01 15:15:33 +0100

    chore(deps): Update snap-storage to 0.6.4
    Now provider tests run without having to pass the `--no-check` flag.

--- providers/Bandcamp/mod.test.ts
+++ providers/Bandcamp/mod.test.ts
@@ -4,13 +4,6 @@ import { describe } from '@std/testing/bdd';
 
 import BandcampProvider from './mod.ts';
 
-// FIXME: `deno test mod.test.ts` fails for Deno 2.1 because of a (.d.mts type import!) dependency of `snap_storage`:
-// error: Expected a JavaScript or TypeScript module, but identified a Unknown module. Importing these types of modules is currently not supported.
-//   Specifier: https://deno.land/std@0.224.0/crypto/_wasm/lib/deno_std_wasm_crypto.generated.d.mts
-//   at https://deno.land/std@0.224.0/crypto/_wasm/lib/deno_std_wasm_crypto.generated.mjs:5:22
-// -> https://github.com/denoland/deno/issues/22889
-// But `deno check mod.test.ts`, `deno run mod.test.ts` and `deno test --no-check mod.test.ts` run just fine!?
-
 describe('Bandcamp provider', () => {
 	using _fetchStub = stubFetchWithCache();
 	const bc = new BandcampProvider(makeProviderOptions());
