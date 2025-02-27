d53768655a83e334b271018299e9f7eb3414b757
David Kellner <52860029+kellnerd@users.noreply.github.com>
2025-02-27 12:47:18 +0100

    WIP

--- providers/Hoerspielforscher/mapping.ts
+++ providers/Hoerspielforscher/mapping.ts
@@ -3,6 +3,7 @@ import type { MediumFormat } from '@/harmonizer/types.ts';
 export const mediumFormatMap: Record<string, MediumFormat | undefined> = {
 	'Audio CD': 'CD',
 	'Audio-CD': 'CD',
+	'Audio-Dateien': 'Digital Media',
 	'12"-Vinyl, 33 1/3 rpm': '12" Vinyl',
 	'Musik-Cassette': 'Cassette',
 };
