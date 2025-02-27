a07fa9f3f19a2e693497d9e31c82bd32105d2773
David Kellner <52860029+kellnerd@users.noreply.github.com>
2025-02-27 17:14:52 +0100

    fix: Correct copy-pasted comment

--- server/dev.ts
+++ server/dev.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
 
-// Automatically load .env environment variable file and configure logger (before anything else).
+// Automatically load .env environment variable file (before anything else).
 import 'std/dotenv/load.ts';
 
 import dev from 'fresh/dev.ts';
