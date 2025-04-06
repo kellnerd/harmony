4846d682645a5318bebaabf4b8447fc1a0ec894a
David Kellner <52860029+kellnerd@users.noreply.github.com>
2025-04-06 18:12:31 +0200

    feat(web): Expose and explain lookup defaults on the settings page

--- server/routes/settings.tsx
+++ server/routes/settings.tsx
@@ -1,11 +1,35 @@
+import { defaultProviders } from '@/providers/mod.ts';
+import { ProviderCheckboxes } from '@/server/components/ProviderInput.tsx';
 import { TextSettingRow } from '@/server/components/SettingRow.tsx';
 import { Tooltip } from '@/server/components/Tooltip.tsx';
 import { PersistentTextInput } from '@/server/islands/PersistentInput.tsx';
+import { defaultRegions } from '@/server/state.ts';
 
 export default function Settings() {
 	return (
 		<main>
 			<h2>Settings</h2>
+			<h3>Lookup Defaults</h3>
+			<p>Providers which should be looked up by barcode:</p>
+			<ProviderCheckboxes enabledProviders={defaultProviders} persistent />
+			<p>
+				Some providers only do lookups for a specific region. Specify the region which should be tried if the looked up
+				URL doesn’t already contain a region.
+			</p>
+			<div class='row'>
+				<label class='col' for='region-input'>Region:</label>
+				<Tooltip>
+					<a href='https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2'>ISO 3166-1 alpha-2</a>{' '}
+					country codes (comma-separated)
+				</Tooltip>
+				<PersistentTextInput
+					name='region'
+					id='region-input'
+					namespace='persist'
+					initialValue={defaultRegions.join(',')}
+					useCookie
+				/>
+			</div>
 			<h3>Importer Behavior</h3>
 			<TextSettingRow name='seeder.target' label='Seeder target' defaultValue='_blank'>
 				<Tooltip>
