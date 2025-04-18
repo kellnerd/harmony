kellnerd/harmony

commit 87d33377add62785c7f774d5348988a5b8cc99a4

refactor(web): Turn setting row with optional tooltip into a component

server/components/SettingRow.tsx
@@ -1,0 +1,28 @@
+import { PersistentTextInput } from '@/server/islands/PersistentInput.tsx';
+import type { ComponentChildren } from 'preact';
+
+export function TextSettingRow({ name, label, defaultValue, useCookie, children }: {
+	/** Name of the setting, has to be unique. */
+	name: string;
+	/** Label of the text input. */
+	label: string;
+	/** Default value of the setting. */
+	defaultValue?: string;
+	/** Additionally store the value as cookie, making it available to the server. */
+	useCookie?: boolean;
+	/** Tooltip which is inserted before the input. */
+	children?: ComponentChildren;
+}) {
+	return (
+		<div class='row'>
+			<label class='col' for={name}>{label}:</label>
+			{children ?? <span class='tooltip-anchor' />}
+			<PersistentTextInput
+				name={name}
+				id={name}
+				initialValue={defaultValue}
+				useCookie={useCookie}
+			/>
+		</div>
+	);
+}

server/static/harmony.css
@@ -154,6 +154,10 @@ form.center > div.row {
 	justify-content: center;
 }
 
+div.row > label.col {
+	width: 10em;
+}
+
 input {
 	padding: var(--input-padding);
 	box-sizing: border-box;
@@ -413,14 +417,14 @@ span.tooltip-anchor {
 	position: relative;
 	font-size: 1rem;
 	font-weight: normal;
-	cursor: pointer;
+	height: var(--icon-size);
+	width: var(--icon-size);
 	--arrow-length: 15px;
 	--icon-size: 24px;
 }
 
 span.tooltip-anchor > svg.icon {
-	height: var(--icon-size);
-	width: var(--icon-size);
+	cursor: pointer;
 }
 
 div[role='tooltip'] {

server/routes/settings.tsx
@@ -1,3 +1,4 @@
+import { TextSettingRow } from '@/server/components/SettingRow.tsx';
 import { Tooltip } from '@/server/components/Tooltip.tsx';
 import { PersistentTextInput } from '@/server/islands/PersistentInput.tsx';
 
@@ -6,8 +7,7 @@ export default function Settings() {
 		<main>
 			<h2>Settings</h2>
 			<h3>Importer Behavior</h3>
-			<div class='row'>
-				<label for='seeder.target'>Seeder target:</label>
+			<TextSettingRow name='seeder.target' label='Seeder target' defaultValue='_blank'>
 				<Tooltip>
 					Name of the <a href='https://developer.mozilla.org/en-US/docs/Web/HTML/Element/form#target'>target</a>{' '}
 					browsing context of the import button:
@@ -20,8 +20,7 @@ export default function Settings() {
 						</li>
 					</ul>
 				</Tooltip>
-				<PersistentTextInput name='seeder.target' id='seeder.target' initialValue='_blank' />
-			</div>
+			</TextSettingRow>
 		</main>
 	);
 }

commit 2a4a2beca2387457aef037486cd5aa976dab08ee

style(web): Only assign fixed width to tooltip anchor placeholders
Fixes a style regression from 87d33377add62785c7f774d5348988a5b8cc99a4,
which broke the alignment of the landing page tooltip on small screens.

server/components/SettingRow.tsx
@@ -18,7 +18,7 @@ export function TextSettingRow({ name, label, defaultValue, useCookie, children 
 	return (
 		<div class='row'>
 			<label class='col' for={name}>{label}:</label>
-			{children ?? <span class='tooltip-anchor' />}
+			{children ?? <span class='tooltip-anchor placeholder' />}
 			<PersistentTextInput
 				name={name}
 				id={name}

server/static/harmony.css
@@ -417,10 +417,13 @@ span.tooltip-anchor {
 	position: relative;
 	font-size: 1rem;
 	font-weight: normal;
-	height: var(--icon-size);
-	width: var(--icon-size);
 	--arrow-length: 15px;
 	--icon-size: 24px;
+}
+
+span.tooltip-anchor.placeholder {
+	height: var(--icon-size);
+	width: var(--icon-size);
 }
 
 span.tooltip-anchor > svg.icon {