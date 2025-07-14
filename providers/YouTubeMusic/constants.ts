b18e07c5a3310feebb21db1d7bd5bac7eb26a2e9
Fea <feathecutie@disroot.org>
2025-07-14 23:27:45 +0200

    chore(YouTube): Clean up request body

--- providers/YouTubeMusic/constants.ts
+++ providers/YouTubeMusic/constants.ts
@@ -4,6 +4,8 @@ export const SEARCH_URL = new URL('https://www.youtube.com/youtubei/v1/search?pr
 export const USER_AGENT =
 	'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36';
 
+const CLIENT_VERSION = '1.20250219.01.00';
+
 export const YOUTUBEI_HEADERS = {
 	accept: '*/*',
 	'accept-language': '*',
@@ -11,42 +13,16 @@ export const YOUTUBEI_HEADERS = {
 	origin: 'https://www.youtube.com',
 	'user-agent': USER_AGENT,
 	'x-youtube-client-name': '67',
-	'x-youtube-client-version': '1.20250219.01.00',
+	'x-youtube-client-version': CLIENT_VERSION,
 };
 
 export const YOUTUBEI_BODY = {
-	isAudioOnly: true,
 	context: {
 		client: {
 			hl: 'en',
 			gl: 'US',
-			screenDensityFloat: 1,
-			screenHeightPoints: 1440,
-			screenPixelDensity: 1,
-			screenWidthPoints: 2560,
 			clientName: 'WEB_REMIX',
-			clientVersion: '1.20250219.01.00',
-			osName: 'Windows',
-			osVersion: '10.0',
-			userAgent:
-				'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-			platform: 'DESKTOP',
-			clientFormFactor: 'UNKNOWN_FORM_FACTOR',
-			userInterfaceTheme: 'USER_INTERFACE_THEME_LIGHT',
-			deviceMake: '',
-			deviceModel: '',
-			browserName: 'Edge Chromium',
-			browserVersion: '109.0.1518.61',
-			utcOffsetMinutes: 120,
-			memoryTotalKbytes: '8000000',
-			mainAppWebInfo: {
-				graftUrl: 'https://www.youtube.com',
-				pwaInstallabilityStatus: 'PWA_INSTALLABILITY_STATUS_UNKNOWN',
-				webDisplayMode: 'WEB_DISPLAY_MODE_BROWSER',
-				isWebNativeShareAvailable: true,
-			},
+			clientVersion: CLIENT_VERSION,
 		},
-		user: { enableSafetyMode: false, lockedSafetyMode: false },
-		request: { useSsl: true, internalExperimentFlags: [] },
 	},
 };
