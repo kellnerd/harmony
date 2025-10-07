62a29e67782a28768b7cdd4d23fd0a9c9e15e1af
Gustav Utterheim <gustav@utterheim.com>
2025-10-07 19:46:53 +0200

    refactor: Add delay between pop-ups opening

--- server/islands/OpenAllLinks.tsx
+++ server/islands/OpenAllLinks.tsx
@@ -3,24 +3,61 @@ import { Button } from '@/server/components/Button.tsx';
 import type { EntityType } from '@kellnerd/musicbrainz/data/entity';
 import { useSignal } from '@preact/signals';
 
+/**
+ * Delay between every pop-up, in milliseconds.
+ */
+const popUpDelay = 300;
+const blockedPopUpMessage = 'Pop-up was blocked';
+
 export function OpenAllLinks({ mbEditLinks, entityType }: {
 	mbEditLinks: string[];
 	entityType: EntityType;
 }) {
 	const showBlockedPopUpWarning = useSignal(false);
+	const numberOfLinks = mbEditLinks.length;
 	// Only show the button if there are multiple links to open.
-	if (mbEditLinks.length <= 1) return null;
+	if (numberOfLinks <= 1) return null;
 
-	function openAllLinks() {
-		mbEditLinks.forEach((mbEditLink) => {
-			if (mbEditLink) {
-				const status = globalThis.open(mbEditLink, '_blank');
-				if (!status && !showBlockedPopUpWarning.value) {
-					// Show warning if the browser blocked the pop-up.
-					showBlockedPopUpWarning.value = true;
-				}
+	async function openAllLinks() {
+		const controller = new AbortController();
+		const signal = controller.signal;
+
+		const newWindowPromises = mbEditLinks.map((mbEditLink, index) =>
+			new Promise<void>((resolve, reject) => {
+				if (!mbEditLink) return resolve();
+
+				const timeoutId = setTimeout(() => {
+					const newWindow = globalThis.open(mbEditLink, '_blank');
+					const isBlocked = !newWindow || newWindow.closed || typeof newWindow.closed === 'undefined';
+
+					if (isBlocked) {
+						// If the pop-up was blocked, reject the promise with a specific message.
+						return reject(new Error(blockedPopUpMessage));
+					}
+
+					return resolve();
+				}, index * popUpDelay);
+
+				// If the controller is aborted, clear the timeout and reject the promise.
+				signal.addEventListener('abort', () => {
+					clearTimeout(timeoutId);
+					reject();
+				});
+			})
+		);
+
+		try {
+			await Promise.all(newWindowPromises);
+			// If all pop-ups were opened, hide warning if it was visible.
+			showBlockedPopUpWarning.value = false;
+		} catch (error) {
+			if (error instanceof Error && error.message === blockedPopUpMessage) {
+				// Abort all pending pop-ups.
+				controller.abort();
+				// Show warning if the browser blocked the pop-up.
+				showBlockedPopUpWarning.value = true;
 			}
-		});
+		}
 	}
 
 	return (
