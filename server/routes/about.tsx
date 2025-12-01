e4512fad755c7ca4d3c60dadbc0040b5bbcd2a7c
David Kellner <52860029+kellnerd@users.noreply.github.com>
2025-12-01 13:45:15 +0100

    feat(web): Document release lookup parameters and category values

--- server/routes/about.tsx
+++ server/routes/about.tsx
@@ -1,5 +1,7 @@
 import { providers } from '@/providers/mod.ts';
+import { categoryDescriptions } from '@/providers/categories.ts';
 import { FeatureQuality } from '@/providers/features.ts';
+import { defaultRegions } from '@/server/state.ts';
 import { ProviderIcon } from '@/server/components/ProviderIcon.tsx';
 import { Head } from 'fresh/runtime.ts';
 
@@ -9,6 +11,52 @@ export default function About() {
 			<Head>
 				<title>About Harmony</title>
 			</Head>
+			<h2>Release Lookup Parameters</h2>
+			<p>The Release Lookup page (and the Release Actions page) accept the following query parameters:</p>
+			<dl>
+				<dt>
+					<code>url</code>
+				</dt>
+				<dd>
+					Release URL from one of the supported providers. Can be specified multiple times to lookup multiple sources.
+				</dd>
+				<dt>
+					<code>gtin</code>
+				</dt>
+				<dd>
+					Barcode (GTIN, EAN or UPC) of the release. Can be specified instead of or in addition to <code>url</code>.
+				</dd>
+				<dt>
+					<code>region</code>
+				</dt>
+				<dd>
+					<a href='https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2'>ISO 3166-1 alpha-2</a>{' '}
+					country code for providers with region-specific pages or API. Multiple values can be specified as fallbacks.
+					The default regions ({defaultRegions.join(', ')}) can be changed in the settings.
+				</dd>
+				<dt>
+					<code>category</code>
+				</dt>
+				<dd>
+					Category of the (additional) providers which should be looked up by barcode. Supported values:
+					<dl>
+						{Object.entries(categoryDescriptions).map(([category, description]) => (
+							<>
+								<dt>
+									<code>{category}</code>
+								</dt>
+								<dd>
+									{description} Includes: {providers.filterInternalNamesByCategory(category).join(', ') || '?'}
+								</dd>
+							</>
+						))}
+					</dl>
+				</dd>
+			</dl>
+			<p>
+				In addition to the predefined categories, any provider can be added to the combined lookup by appending its
+				internal name as a query parameter without value (or even with a release ID as value).
+			</p>
 			<h2>Supported Providers</h2>
 			<table>
 				<tr>
